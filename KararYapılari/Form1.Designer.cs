8142bfe3e2b242f2b164b2a40a8a223b0253c7d6
Mustafa Emre Ilhan <mustafaemreilhan1903@gmail.com>
2021-05-26 10:11:12 +0300

    indirimli tutar hesaplama

--- KararYapılari/Form1.Designer.cs
+++ KararYapılari/Form1.Designer.cs
@@ -41,6 +41,17 @@ namespace KararYapılari
             this.button2 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
@@ -144,11 +155,120 @@ namespace KararYapılari
             this.textBox2.Size = new System.Drawing.Size(170, 26);
             this.textBox2.TabIndex = 12;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(330, 194);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 36);
+            this.button3.TabIndex = 23;
+            this.button3.Text = "Hesapla";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(299, 15);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 20);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Kitap Adet";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(396, 12);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(170, 26);
+            this.textBox6.TabIndex = 21;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(326, 126);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(107, 20);
+            this.label7.TabIndex = 24;
+            this.label7.Text = "İndirimli Tutar:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(451, 126);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(40, 20);
+            this.label9.TabIndex = 25;
+            this.label9.Text = "00 ₺";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(451, 64);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(40, 20);
+            this.label8.TabIndex = 27;
+            this.label8.Text = "00 ₺";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(326, 64);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(104, 20);
+            this.label10.TabIndex = 26;
+            this.label10.Text = "Normal Tutar:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(326, 95);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(102, 20);
+            this.label11.TabIndex = 28;
+            this.label11.Text = "İndirim Fiyat :";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(451, 95);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(40, 20);
+            this.label12.TabIndex = 29;
+            this.label12.Text = "00 ₺";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(451, 156);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(32, 20);
+            this.label13.TabIndex = 31;
+            this.label13.Text = "0%";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(326, 156);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(108, 20);
+            this.label14.TabIndex = 30;
+            this.label14.Text = "İndirimli Oranı:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.label4);
@@ -182,6 +302,17 @@ namespace KararYapılari
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
     }
 }
 
--- KararYapılari/Form1.cs
+++ KararYapılari/Form1.cs
@@ -15,7 +15,7 @@ namespace KararYapılari
         public Form1()
         {
             InitializeComponent();
-        }
+        }       
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -46,8 +46,56 @@ namespace KararYapılari
             {
                 durum = "kaldı";
             }
-            textBox5.Text = ort.ToString("0.00")+"/" + durum;
-            
+            textBox5.Text = ort.ToString("0.00")+"/" + durum;          
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //0-20 ==> %20 
+            //21-40 ==> %40 
+            //41++ ==> %50 
+            //Kitap 8 ₺
+
+            int kitapadet, indirimorani;
+            double toplam,normaltp,indirimli;
+            kitapadet = Convert.ToInt16(textBox6.Text);
+            normaltp = kitapadet * 8;
+            label8.Text = normaltp + "₺";
+            if (kitapadet >=0 && kitapadet <=20)
+            {
+                toplam = (kitapadet * 8) - (kitapadet * 8 * 0.20);
+                label9.Text = toplam + "₺";
+                indirimli = kitapadet * 8 * 0.20;               
+                label12.Text = indirimli + "₺";
+                indirimorani = 20;
+                label13.Text = "%" + indirimorani;
+            }
+
+            else if (kitapadet >= 21 && kitapadet <= 40)
+            {
+                toplam = (kitapadet * 8) - (kitapadet * 8 * 0.40);
+                label9.Text = toplam + "₺";
+                indirimli = kitapadet * 8 * 0.40;
+                label12.Text = indirimli + "₺";
+                indirimorani = 40;
+                label13.Text = "%" + indirimorani;
+            }
+
+            else if (kitapadet >= 41)
+            {
+                toplam = (kitapadet * 8) - (kitapadet * 8 * 0.50);
+                label9.Text = toplam + "₺";
+                indirimli = kitapadet * 8 * 0.50;
+                label12.Text = indirimli + "₺";
+                indirimorani = 50;
+                label13.Text = "%" + indirimorani;
+            }
+            else
+            {
+                label9.Text = "Lütfen 0 dan küçük değer girmeyeniz";
+            }
+
 
         }
     }
